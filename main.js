c22a065915e18b5b9dfa156e0efa6f8c47dccfb9
The_Grits <gritsprogramming@gmail.com>
2016-01-10 11:46:57 -0500

    Added functions
    Got the easy stuff out of the way

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 var studioid = 1788915;
+
+function getLastComment(){
+  
+}
+
+function formatLink(lastCommentId){
+  return "scratch.mit.edu/studios/" + studioid + "/comments/#comments-" + lastCommentId; 
+}
+
+function changeLink(link){
+  document.getElementById("link").href=link;
+}
