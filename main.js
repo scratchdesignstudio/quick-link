460fe3846231803fb1c4fe7795df28ff83c52081
Technoboy10 <technoboy10@fake.email>
2016-01-12 22:59:38 -0700

    fleshed out comment detection more

--- main.js
+++ main.js
@@ -1,27 +1,34 @@
 var studioid = 1788915;
-
+var curators = ["technoboy10", "The_Grits", "4LeafClovR", "puppymk", "Malik44", "CrazyNimbus", "fmtfmtfmt2", "GreenIeaf", "st19_galla"]; //Probably more than this
+var foobar;
 function getLastComment(page){
-  var replyList;
+  var commentList;
   //Grab
   var xml = new XMLHttpRequest();
   xml.onreadystatechange = function(){
     if (xml.readyState = 4){
       var container = document.implementation.createHTMLDocument().documentElement;
       container.innerHTML = xml.responseText;
-      replyList = container.querySelectorAll('.top-level-reply > .replies');
+      commentList = Array.from(container.querySelectorAll('.top-level-reply > .replies')).reverse();
     }
   }
   xml.open("GET", "https://crossorigin.me/https://scratch.mit.edu/site-api/comments/gallery/" + studioid + "/?page=" + page , false);
   xml.send(null);
 
   //Parse through array
-  for (i = 0; i < replyList.length; i++){
-    var commentList = replyList[i].querySelectorAll('.reply > .comment'); //Get all comments
-    for (j = 0; j < commentList.length; j++){
-      if(commentList[j].querySelector(".info > .name > a").innerHTML == "technoboy10"){ //pick comment by me
-        console.log(commentList[j].querySelector('.info > .content').textContent); //log comment
+  var lastReply = -1;
+  for (i = 0; i < commentList.length; i++){
+    var replyList = commentList[i].querySelectorAll('.reply > .comment'); //Get all comments
+    for (j = 0; j < replyList.length; j++){ //go through replies
+      if(curators.indexOf(replyList[j].querySelector(".info > .name > a").innerHTML) != -1){ //pick comment by a curator
+        lastReply = i;
       }
     }
+  }
+  if (lastReply == commentList.length-1){
+    return false; //All comments on page have been checked, so this should return the first comment (commentList[0]) if the next page *isn't* done
+  } else {
+    return commentList[lastReply+1].getAttribute('data-thread'); // last unread link
   }
 
 }
